4ff4d47341bb801c8a26b0bf52dbaad5a0d61134
Fonserbc <fonserbc@gmail.com>
2018-01-26 21:58:30 +0100

    Initial script files

---
+++ Assets/Scripts/Logic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour {
+
+    public Constants c;
+
+    public GameState currentState;
+	
+	void Start () {
+		
+	}
+	
+    public void UpdateState (InputState input)
+    {
+		
+	}
+}
---
+++ Assets/Scripts/Visuals.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visuals : MonoBehaviour {
+	
+	void Start ()
+    {
+		
+	}
+	
+    void UpdateFrom (GameState state)
+    {
+		
+	}
+}
