9c00d63c4c11c6dd9cf146f39406f341478c5663
Fonserbc <fonserbc@gmail.com>
2018-01-26 22:23:36 +0100

    Working on game and input states

--- Assets/Scripts/InputState.cs
+++ Assets/Scripts/InputState.cs
@@ -1,3 +1,8 @@
 ﻿public struct InputState {
-    
+    public struct PlayerInput {
+        public float xAxis, yAxis;
+        public bool up, down, left, right;
+    }
+
+    public PlayerInput[] playerInputs;
 }
