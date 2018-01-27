343b7b50b675702d29943bcba243927f779ee463
Fonserbc <fonserbc@gmail.com>
2018-01-27 17:14:33 +0100

    Derp

--- Assets/Scripts/Physics2DTest.cs
+++ Assets/Scripts/Physics2DTest.cs
@@ -12,14 +12,14 @@ public class Physics2DTest : MonoBehaviour {
 	void Start () {
         GameState state = new GameState();
         state.players = new GameState.PlayerInfo[2];
-        state.players[0].stunned = 1;
+        state.players[0].stunnedTime = 1f;
 
         GameState copyState = new GameState(state);
 
 
-        copyState.players[0].stunned = 0;
+        copyState.players[0].stunnedTime = 0;
 
-        if (copyState.players[0].stunned == state.players[0].stunned) {
+        if (copyState.players[0].stunnedTime == state.players[0].stunnedTime) {
             Debug.Log("L'array no es copia");;
         }
 	}
