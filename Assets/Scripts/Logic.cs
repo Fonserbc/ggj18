b0b3f142a4e987ad57a45b4231b3fd96b9bfbab4
Fonserbc <fonserbc@gmail.com>
2018-01-27 01:57:49 +0100

    Changing what functions return

--- Assets/Scripts/Logic.cs
+++ Assets/Scripts/Logic.cs
@@ -2,40 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Logic : MonoBehaviour {
+public class Logic : MonoBehaviour
+{
 
     public Constants c;
-
-    public GameState currentState;
-    public InputState currentInput;
 
     GameState newState;
 
     public GameObject level;
 
     public BoxCollider2D[] staticWorld;
-	
-	void Start () {
-        staticWorld = level.GetComponentsInChildren<BoxCollider2D>();
-	}
-	
-    public void UpdateState (InputState newInput)
+
+    void Start()
     {
-        newState = currentState;
+        staticWorld = level.GetComponentsInChildren<BoxCollider2D>();
+    }
+
+    public GameState UpdateState(GameState previousState, InputState previousInput, InputState newInput)
+    {
+        newState = previousState;
 
         // PlayerInput
         for (int i = 0; i < c.numPlayers; ++i)
         {
             UpdatePlayerPos(i, newInput.playerInputs[i]);
 
-            UpdatePlayerActions(i, currentInput.playerInputs[i], newInput.playerInputs[i]);
+            UpdatePlayerActions(i, previousInput.playerInputs[i], newInput.playerInputs[i]);
         }
 
         // Physics Collision
         for (int i = 0; i < c.numPlayers; ++i)
         {
             // Against static world
-            for (int j = 0; j < staticWorld.Length; ++j) {
+            for (int j = 0; j < staticWorld.Length; ++j)
+            {
                 if (CircleAABBCollides(newState.players[i].position, c.playerCollisionRadius, staticWorld[j].bounds))
                 {
                     newState.players[i].position = CircleAABBCorrect(newState.players[i].position, c.playerCollisionRadius, staticWorld[j].bounds);
@@ -43,32 +43,57 @@ public class Logic : MonoBehaviour {
             }
 
             // Against antenas
+            for (int j = 0; j < newState.antenas.Length; ++j)
+            {
+                if (CircleCircleCollides(newState.players[i].position, c.playerCollisionRadius, newState.antenas[j].position, c.antenaCollisionRadius))
+                {
+                    newState.players[i].position = CircleCircleCorrect(newState.players[i].position, c.playerCollisionRadius, newState.antenas[j].position, c.antenaCollisionRadius);
+                }
+            }
 
+            // Against other players
+            for (int j = 0; j < newState.players.Length; ++j)
+            {
+                if (i != j && CircleCircleCollides(newState.players[i].position, c.playerCollisionRadius, newState.players[j].position, c.playerCollisionRadius))
+                {
+                    newState.players[i].position = CircleCircleCorrect(newState.players[i].position, c.playerCollisionRadius, newState.players[j].position, c.playerCollisionRadius);
+                }
+            }
         }
 
         // World Logic
-	}
+        // Electricity/antenagrabbing/whatever
 
-    void UpdatePlayerPos(int id, InputState.PlayerInput input) {
-        if (newState.players[id].stunned > 0) {
+        return newState;
+    }
+
+    void UpdatePlayerPos(int id, InputState.PlayerInput input)
+    {
+        if (newState.players[id].stunned > 0)
+        {
             newState.players[id].stunned--;
             return;
         }
 
-        if (!newState.players[id].connected) {
+        if (!newState.players[id].connected)
+        {
             return;
         }
         Vector2 axis = new Vector2(input.xAxis, input.yAxis);
-        if (axis.x == 0.0f && axis.y == 0.0f) {
+        if (axis.x == 0.0f && axis.y == 0.0f)
+        {
             return;
         }
 
-        if (axis.magnitude > 1f) {
+        if (axis.magnitude > 1f)
+        {
             axis.Normalize();
         }
 
         newState.players[id].position.x += axis.x * c.playerSpeed * c.fixedDeltaTime;
         newState.players[id].position.y += axis.y * c.playerSpeed * c.fixedDeltaTime;
+
+        newState.players[id].rotation = Vector2.SignedAngle(Vector2.right, axis);
     }
 
     void UpdatePlayerActions(int id, InputState.PlayerInput lastInput, InputState.PlayerInput newInput)
@@ -95,21 +120,32 @@ public class Logic : MonoBehaviour {
         }
     }
 
-    int FindAntenaNearPlayer (Vector2 playerPos) {
-        for (int i = 0; i < newState.antenas.Length; ++i) {
+    int FindAntenaNearPlayer(Vector2 playerPos)
+    {
+        for (int i = 0; i < newState.antenas.Length; ++i)
+        {
             if (Vector2.Distance(playerPos, newState.antenas[i].position) < c.antenaActivationRadius) return i;
         }
         return -1;
     }
 
-    bool CircleAABBCollides (Vector2 cPos, float r, Bounds aabb) {
-
+    bool CircleAABBCollides(Vector2 cPos, float r, Bounds aabb)
+    {
         return false;
     }
 
     Vector2 CircleAABBCorrect(Vector2 cPos, float r, Bounds aabb)
     {
-
         return cPos;
+    }
+
+    bool CircleCircleCollides(Vector2 c1, float r1, Vector2 c2, float r2)
+    {
+        return false;
+    }
+
+    Vector2 CircleCircleCorrect(Vector2 movingCenter, float movingRadius, Vector2 staticCenter, float staticRadius)
+    {
+        return movingCenter;
     }
 }
